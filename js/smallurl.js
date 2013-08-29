c289165ecfc91924e3aa024df5cfd58f4334025a
Thomas Edwards <tmfksoft@gmail.com>
2013-08-29 04:50:06 +0400

    Added Files

---
+++ js/smallurl.js
@@ -0,0 +1,65 @@
+/* SmallURL Javascript Core by Thomas Edwards
+ * Copyright 2013 SmallURL
+ * Do not attempt to exploit the SmallURL Service
+*/
+function call_shortener(do_custom) {
+	var url = encodeURIComponent($('#long_url').val());
+	if (!do_custom) {
+		var api_url = "http://smallurl.in/api/api.php?url="+url;
+	}
+	else {
+		var custom = $('#custom_url').val();
+		var api_url = "http://smallurl.in/api/api.php?url="+url+"&custom="+custom;
+	}
+	$.getJSON(api_url,function (data) {
+		if (data.res == true) {
+			$('#alert').hide(); // Just incase someone got an error last time.
+			do_visuals(data);
+		}
+		else {
+			$('#alert_error').html(data.msg);
+			$('#alert').show();
+		}
+	});
+}
+function do_visuals(url) {
+	$('#short_url').val('http://smallurl.in/'+url.short)
+	$('#form').hide();
+	$('#shortened').show();
+	$('#short_url').select();
+}
+function reset_form() {
+	// Reset all fields.
+	$('#short_url').val('');
+	$('#long_url').val('');
+	$('#custom_url').val('');
+	$('#custom_btn').show();
+	// Reset all forms.
+	$('#alert').hide();
+	$('#custom_form').hide();
+	$('#shortened').hide();
+	$('#form').show();
+}
+function show_custom() {
+	$('#custom_url').val('');
+	$('#custom_btn').hide();
+	$('#custom_form').show();
+}
+function validateText(str) {
+	var tarea = str;
+	if (tarea.indexOf("http://")==0 && tarea.indexOf("https://")==0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+   scanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
---
+++ src/com/saatcioglu/phonegap/clipboardmanager/ClipboardManagerPlugin.java
@@ -0,0 +1,70 @@
+/**
+ * Phonegap ClipboardManager plugin
+ * Omer Saatcioglu 2011
+ * enhanced by Guillaume Charhon - Smart Mobile Software 2011
+ * ported to Phonegap 2.0 by Jacob Robbins
+ */
+
+package com.saatcioglu.phonegap.ClipboardManagerPlugin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.text.ClipboardManager;
+
+
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.PluginResult;
+
+
+public class ClipboardManagerPlugin extends CordovaPlugin {
+	private static final String actionCopy = "copy";
+	private static final String actionPaste = "paste";
+	private static final String errorParse = "Couldn't get the text to copy";
+	private static final String errorUnknown = "Unknown Error";
+
+	private ClipboardManager mClipboardManager;
+
+
+	@Override
+	  public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
+	  {
+	
+		// If we do not have the clipboard
+		if(mClipboardManager == null) {
+			mClipboardManager = (ClipboardManager) cordova.getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+		}
+		
+	
+		// Copy
+		if (action.equals(actionCopy)) {
+			String arg = "";
+			try {
+				arg = (String) args.get(0);
+				mClipboardManager.setText(arg);
+			} catch (JSONException e) {
+			      callbackContext.error( errorParse);
+			} catch (Exception e) {
+			      callbackContext.error( errorUnknown);
+			}
+			callbackContext.success();
+		// Paste
+		} else if (action.equals(actionPaste)) {
+			String arg = (String) mClipboardManager.getText();
+			if (arg == null) {
+				arg = "";
+			}
+			PluginResult copy_ret = new PluginResult(PluginResult.Status.OK, arg);
+			callbackContext.sendPluginResult(copy_ret);
+			callbackContext.success();
+			
+		} else {
+		      callbackContext.error("invalid action");
+		      return false;
+		}
+
+		return true;
+	 }
+}
